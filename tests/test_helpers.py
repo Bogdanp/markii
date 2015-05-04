a030dd33da80839bb656953b692fd7bd0fdf7bf9
Bogdan Popa <popa.bogdanp@gmail.com>
2015-05-04 16:04:21 -0500

    add tests

---
+++ tests/test_helpers.py
@@ -0,0 +1,73 @@
+import bootstrap  # noqa
+
+
+def test_getrusage():
+    from markii.markii import getrusage
+
+    try:
+        import resource  # noqa
+        assert getrusage()
+    except ImportError:
+        assert getrusage() is None
+
+
+def test_getprocinfo():
+    from markii.markii import resource, getprocinfo
+
+    process = getprocinfo()
+    if not resource:
+        assert process is None
+
+    assert "utime" in process
+    assert "stime" in process
+    assert "mem" in process
+
+    assert isinstance(process.get("utime"), basestring)
+    assert isinstance(process.get("stime"), basestring)
+    assert isinstance(process.get("mem"), basestring)
+
+
+def test_deident():
+    from markii.markii import deindent
+    source = """\
+    def foo():
+        return 42
+"""
+    target = """\
+def foo():
+    return 42
+"""
+    assert deindent(source) == target
+
+
+def test_deident_unindented():
+    from markii.markii import deindent
+    source = """\
+def foo():
+    return 42
+"""
+    assert deindent(source) == source
+
+
+def test_getframes():
+    from markii.markii import getframes
+
+    def f():
+        raise Exception()
+
+    def g():
+        return f()
+
+    def h():
+        return g()
+
+    try:
+        h()
+    except:
+        frames = getframes()
+        assert frames
+        assert len(frames) == 4
+        assert frames[0].func == "f"
+        assert frames[1].func == "g"
+        assert frames[2].func == "h"
+        assert frames[3].func == "test_getframes"
