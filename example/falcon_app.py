f059660659fefe5423734c04259f23e9c2263492
Bogdan Popa <popa.bogdanp@gmail.com>
2017-03-08 16:19:52 +0200

    fix route in falcon example

--- example/falcon_app.py
+++ example/falcon_app.py
@@ -7,14 +7,14 @@ from paste import httpserver
 
 
 class Handler(object):
-    def on_get(self, request, response, n):
-        response.body = str(int(n))
+    def on_get(self, request, response, num):
+        response.body = str(int(num))
 
 
 root = os.path.abspath(os.path.dirname(__file__))
 app = falcon.API()
 app.add_error_handler(Exception, partial(handle_error, app_root=root))
-app.add_route("/{n}", Handler())
+app.add_route("/{num}", Handler())
 
 if __name__ == "__main__":
     httpserver.serve(app, host="127.0.0.1", port="8080")
